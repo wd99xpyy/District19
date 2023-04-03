197d7f3e87766acad40d5098b76349a32043a7ac
wd99xpyy <wangq26@miamioh.edu>
2023-04-03 11:51:58 -0400

    update

--- script/MainMenu.gd
+++ script/MainMenu.gd
@@ -1,30 +1,20 @@
 extends Node2D
-
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 export(String, FILE, "*.tres") var dialogue = null
 export(String) var speaker
 var convo
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Global.connect("changeSceneToDistrict",self,"loadToDistrict")
+	Global.resetGlobal()
+	Global.connect("changeScene",self,"loadToDistrict")
 	if dialogue != null:
 		convo = load(dialogue)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_Button_pressed():
 	$Button.hide()
 	$Button2.hide()
-	DialogueManager.show_example_dialogue_balloon(speaker, convo)
+	var dia = DialogueManager.show_example_dialogue_balloon(speaker, convo)
 	#get_tree().change_scene("res://scene/District19.tscn")
-
 func loadToDistrict():
 	get_tree().change_scene("res://scene/District19.tscn")
